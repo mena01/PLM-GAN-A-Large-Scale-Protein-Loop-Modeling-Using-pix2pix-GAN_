d4f1ea65a1a0baae60105529ba0a9921816bb08c
mena01 <73284871+mena01@users.noreply.github.com>
2023-09-17 10:00:36 +0300

    update
    update files

--- data set/DownloadDataFromPDB.py
+++ data set/DownloadDataFromPDB.py
@@ -1,3 +1,24 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:cfd500d04487645c582f2bcc0d23fb000f740c905cf980268e3dceab5e0ae50f
-size 547
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 10 12:16:15 2021
+
+@author: menanagy
+"""
+#https://github.com/collinarnett/protein_gan
+
+from pathlib import Path
+from urllib import request
+from tqdm import tqdm
+
+train_folder = Path("/download_dir_proteinsPDB")
+train_list = Path('/train_ids.txt').read_text().splitlines()
+ 
+train_folder.mkdir(exist_ok=True)
+
+for ids in tqdm(test_list):
+    p = test_folder / f'{ids}.pdb'
+    if not p.exists():
+        request.urlretrieve(f"http://files.rcsb.org/download/{ids}.pdb", p)
+ 
+    
